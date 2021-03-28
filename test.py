278495b3f92b07217f65cbdc498b67b6a81d4d26
chun1213 <53665251+chun1213@users.noreply.github.com>
2021-03-27 23:49:52 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import chess.pgn
 import numpy as np
 from tensorflow import keras
 import tensorflow as tf
-model = keras.models.load_model('D:\ChessModels\Chess9.1')
+model = keras.models.load_model('D:\ChessModels\Chess9.2')
 chess_dict = {
     'p' : [1,0,0,0,0,0,0,0,0,0,0,0],
     'P' : [0,0,0,0,0,0,1,0,0,0,0,0],
@@ -90,28 +90,28 @@ def moveValue(engine_Moves,chess_board,initial_move):
         chess_board.push_san(initial_move)
     opmove=0
     for move in engine_Moves:
-        opmove=0
+        opmove=1
         if engine_Moves.index(move)%2==1:
-            opmove=1
+            opmove=-1
         clonemove=move
         if 'x' in move:
+            mod = 1
             x_Pos = move.index('x')
-            move=move.replace("#","")
-            move = move.replace("+","")
+            if '#' in move:
+                move=move.replace("#","")
+                baseValue += 999 * mod * opmove
+            if '+' in move:
+                move = move.replace("+","")
+                baseValue += 1 * mod * opmove
             piecePos = boardIndex(move[x_Pos + 1::])
             piece = chess_board.piece_at(piecePos)
-            mod=1
             if engine_Moves[length-1]==move:
                 mod=0.5
-            if opmove==1:
-                baseValue -= pieceValue(piece)*mod
-                print('hell')
-            else:
-                baseValue += pieceValue(piece)*mod
-                print('hell0')
+            baseValue += pieceValue(piece)*mod*opmove
+                # print('hell')
         chess_board.push_san(str(clonemove))
-    print (engine_Moves)
-    print (baseValue)
+    # print (engine_Moves)
+    # print (baseValue)
     return baseValue
 
 def branching_moves(move,chess_board,depth):
@@ -133,25 +133,100 @@ def branching_moves(move,chess_board,depth):
             move_tree.append(move_line)
     return move_tree
 
+def evalBoard(position,player):
+    score = 0
+    if position.is_checkmate():
+        if (player):
+            return -9999
+        else:
+            return 9999
+    for i in range(64):
+        x=str(position.piece_at(i))
+        if (x=="P"):
+            score+=1
+        elif (x=="p"):
+            score-=1
+        elif (x=="R"):
+            score+=5
+        elif (x=="r"):
+            score-=5
+        elif (x=="N"):
+            score+=3
+        elif (x=="n"):
+            score-=3
+        elif (x=="B"):
+            score+=3
+        elif (x=="b"):
+            score-=3
+        elif (x=="Q"):
+            score+=9
+        elif (x=="q"):
+            score-=9
+    return score
+
+
+def minimax(position, depth, alpha, beta, maximizingPlayer):
+    if depth == 0 or position.is_game_over():
+        return None, evalBoard(position,maximizingPlayer)
+
+    legal_move = str(position.legal_moves)[38:-2].replace(',', '').split()
+    bestMove=legal_move[0]
+    if maximizingPlayer:
+        maxEval = -99999
+        for move in legal_move:
+            board2 = position.copy()
+            board2.push_san(move)
+            eval = minimax(board2, depth - 1, alpha, beta, False)[1]
+            if eval>maxEval:
+                maxEval = eval
+                bestMove=move
+            alpha = max(alpha, eval)
+            if beta <= alpha:
+                break
+        return bestMove, maxEval
+
+    else:
+        minEval = +99999
+        for move in legal_move:
+            board2 = position.copy()
+            board2.push_san(move)
+            eval = minimax(board2, depth - 1, alpha, beta, True)[1]
+            if eval < minEval:
+                minEval = eval
+                bestMove = move
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
+        return bestMove, minEval
+
 def eval_Branches(moveTree,chess_board):
     # always prunes last layer
     maximize=1
     y=1
     highest=-9999
     lowest=9999
+    #alpha beta pruning
+    alpha=99999
+    beta=99999
+    prune=0
+
     highest_moveset=[]
     lowest_moveset=[]
     prevmoveset=[]
     new_movetree=[]
+    #condtion
     while len(moveTree)>1:
+        #loops through the list of movetrees
         for x in range(len(moveTree)):
-            if moveTree[x][:len(moveTree[x])-y]==prevmoveset[:len(moveTree[x])-y] or prevmoveset==[]:
+            #finds if move is in same node as previous O(1)
+            if ((moveTree[x][:len(moveTree[x])-y]==prevmoveset[:len(moveTree[x])-y] and prune==0) or prevmoveset==[]):
+                #finds final value of the movetree O(n)
                 value = moveValue(moveTree[x],chess_board.copy(),'none')
                 prevmoveset=moveTree[x]
                 if maximize==1 and value>highest:
                     highest=value
                     highest_moveset=moveTree[x]
-                if maximize==0 and value<lowest:
+                elif maximize==0 and value<lowest:
                     lowest=value
                     lowest_moveset=moveTree[x]
             else:
@@ -160,7 +235,7 @@ def eval_Branches(moveTree,chess_board):
                     new_movetree.append(highest_moveset)
                     highest_moveset=moveTree[x]
                     highest = value
-                if maximize == 0:
+                elif maximize == 0:
                     new_movetree.append(lowest_moveset)
                     lowest_moveset = moveTree[x]
                     lowest = value
@@ -175,10 +250,10 @@ def eval_Branches(moveTree,chess_board):
         moveTree=new_movetree
         new_movetree=[]
         prevmoveset = []
-        print(maximize)
-        print("highest",highest_moveset)
-        print("lowest",lowest_moveset)
-        print (moveTree)
+        # print(maximize)
+        # print("highest",highest_moveset)
+        # print("lowest",lowest_moveset)
+        # print (moveTree)
         highest = -9999
         lowest = 9999
         highest_moveset = []
@@ -188,39 +263,64 @@ def eval_Branches(moveTree,chess_board):
 
 
 chess_board = chess.Board()
+maximize=True
+currentVal=-99999
+turncount=0
 while (not chess_board.is_stalemate() and not chess_board.is_insufficient_material() and not chess_board.is_game_over() and not chess_board.is_seventyfive_moves()):
+    maximize = True
+    currentVal = -99999
     print(chess_board)
-    # board2=chess_board.copy()
-    # board2.push_san('e4')
-    # board2.push_san('d5')
-    # print (eval_Branches(branching_moves('none',board2.copy(),2),board2))
-    # print (chess_board.piece_at(boardIndex('e1')))
-    val = input("Enter your move: ")
+    matrix = make_matrix(chess_board)
+    print(evalBoard(chess_board,maximize))
+    board = translate(matrix,chess_dict)
+    board = np.array(board)
+    board = np.reshape(board,(1,8,8,12))
+    legal_moves = str(chess_board.legal_moves)[38:-2].replace(',','').split()
+    print (legal_moves)
+    pred = model.predict(board)
+    index = int(round((len(legal_moves)*pred)[0][0]))
     try:
-        chess_board.push_san(val);
-        print(chess_board.legal_moves)
-        matrix = make_matrix(chess_board)
-        board = translate(matrix,chess_dict)
-        board = np.array(board)
-        board = np.reshape(board,(1,8,8,12))
-        legal_moves = str(chess_board.legal_moves)[38:-2].replace(',','').split()
-        pred = model.predict(board)
-        print (pred)
-        index = int(round((len(legal_moves)*pred)[0][0]))
         engine_Move=legal_moves[index]
-        print(engine_Move)
-        board2=chess_board.copy()
-        board2.push_san(engine_Move)
-        val2=moveValue((eval_Branches(branching_moves('none',board2.copy(),2),board2))[0],board2,'none')
-        print (val2)
-        if (val2>1):
-            move=(eval_Branches(branching_moves('none',chess_board.copy(),2),chess_board.copy()))[0]
-            print (move)
-            chess_board.push_san(move[0])
-        else:
-            chess_board.push_san(engine_Move)
-        if chess_board.is_checkmate():
-            break
     except:
-         print("try again invalid")
-         pass
+        engine_Move = legal_moves[len(legal_moves)-1]
+    boardEngine = chess_board.copy()
+    boardEngine.push_san(engine_Move)
+    engine_value=minimax(boardEngine, 3, -99999, +99999, not maximize)[1]
+    boardMini = chess_board.copy()
+    minmaxMove,val2 = minimax(boardMini, 4, -99999, +99999, maximize)
+
+    print(maximize)
+    print(minmaxMove)
+    print(val2)
+    print(engine_Move)
+    print(engine_value)
+
+    if ((maximize and val2>engine_value or not maximize and val2<engine_value) or turncount>5):
+        move=minmaxMove
+        print (move)
+        chess_board.push_san(move)
+    else:
+        chess_board.push_san(engine_Move)
+        print(engine_Move)
+    #selfplay
+    # if (maximize):
+    #     maximize=False
+    #     currentVal=99999
+    # elif (not maximize):
+    #     maximize=True
+    #     currentVal=-99999
+    if chess_board.is_checkmate():
+        break
+    print(chess_board)
+    while True:
+        print(str(chess_board.legal_moves)[38:-2].replace(',','').split())
+        val = input("Enter your move: ")
+        try:
+            chess_board.push_san(val)
+            break
+        except:
+            pass
+    turncount+=1
+    # except:
+    #      print("try again invalid")
+    #      pass
